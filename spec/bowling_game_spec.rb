853d2656c3ff315b2abfb6af2e243cd27584f2b0
JoseRicardoJimenez <joserjc.96@gmail.com>
2021-06-21 14:45:08 -0500

    Update bowling game and implementation TDD Co-authored-by: Angel Oramas <oramasrojas199angelarturo@hotmail.com>

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,26 +1,39 @@
-require 'rspec'
-
 require_relative '../src/bowling_game'
 
 describe 'Bowling Game Behavior' do
+    
     before do
-        @game = bowlingGame.new
+        @game = Bowling.new
     end
-    it('can roll gutter game') do        
+
+    it "create the rotations in the game" do        
         20.times{@game.roll 0}
-        expect{@game.score}.to eq 0
+        expect(@game.score).to eql 0
     end
 
-    it('can roll all ones') do 
+    it "can do all in one score" do 
         20.times{@game.roll 1}
-        expect(@game.score).to eq 20
+        expect(@game.score).to eql 20
     end
 
-    it('can roll a spare') do
-        @game.roll 5
-        @game.roll 5
-        @game.roll 3
+    it "can do a spare" do
+        @game.roll(5)
+        @game.roll(5)
+        @game.roll(3)
         17.times{@game.roll 0}
-        expect(@game.score).to eq 16
+        expect(@game.score).to eql 16
+    end
+
+    it "can do a strike" do
+        @game.roll(10)
+        @game.roll(3)
+        @game.roll(4)
+        16.times{@game.roll 0}
+        expect(@game.score).to eql 24
+    end
+
+    it "can do a perfect game" do
+        12.times{@game.roll(10)}
+        expect(@game.score).to eql 300
     end
 end
--- src/bowling_game.rb
+++ src/bowling_game.rb
@@ -1,29 +1,59 @@
-class bowlingGame
+# frozen_string_literal: true
+
+# def bowling game
+class Bowling
     def initialize
         @rolls = []
+        @score = 0
     end
 
-    def roll_pins
-        @rolls.push pins
+    def roll pins
+        @rolls << pins
     end
 
-    def score
-        result = 0
-        rollIndex = 0
-        10.time do
-            if spare? rollIndex
-                result += @rolls[rollIndex] + @rolls[rollIndex + 1] + @rolls[rollIndex + 2]
-            else
-                result += @rolls[rollIndex] + @rolls[rollIndex + 1]
-            end
-                rollIndex +=2
-            end
+    def score        
+        roll_index = 0
+        10.times do
+        if strike?(roll_index)
+            @score += strike_or_spare_score(roll_index)
+            roll_index += 1
+        elsif spare?(roll_index)
+            @score += strike_or_spare_score(roll_index)
+            roll_index += 2
+        else
+            @score += frame_score(roll_index)
+            roll_index += 2
         end
-        result
+        end
+        @score
     end
 
-    def spare? rollIndex
-        result += @rolls[rollIndex] + @rolls[rollIndex + 1]==10
+    # def points(roll_index)
+    #     @rolls[roll_index] = Random.rand(0..10)
+    # end
+
+    def strike?(roll_index)
+        # points(roll_index)
+        @rolls[roll_index] == 10
     end
 
+    def strike_or_spare_score(roll_index)
+        @rolls[roll_index] + @rolls[roll_index + 1] +  @rolls[roll_index + 2]
+    end
+
+    def spare?(roll_index)
+        # @dif = 10 - @rolls[roll_index]
+        # @rolls[roll_index + 1] = Random.rand(0..@dif)
+        @rolls[roll_index] + @rolls[roll_index + 1] == 10
+    end
+
+    def frame_score(roll_index)
+        @rolls[roll_index] + @rolls[roll_index + 1]
+    end
+
+    # def final_score
+    #     @rolls.each do |_final_score|
+    #         puts @score, '/'
+    #     end
+    # end
 end
